d722e94786dd17ecc38f546f75bba8fa4328ed86
amelia <amelia.l.sun@pwc.com>
2018-12-11 16:56:43 +0800

    throttle function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
-function throttleTransfer () {
 
+function throttleTransfer(fn, time, firstCallWait) {
+    var time = time || 1400
+    var wait = false
+    var firstCallWait = firstCallWait || false
+    var callNow = !firstCallWait && !wait
+    return function () {
+        var context = this
+        var args = arguments
+        if (!wait) {
+            wait = true
+            tm = setTimeout(function () {
+                fn.apply(context,args)
+                tm = null
+                wait = false
+            }, time);
+        }
+        if (callNow) {
+            fn.apply(context,args)
+            callNow = false
+        }
+    }
 }
-module.export = throttleTransfer
+
+module.exports = throttleTransfer
--- test/test.js
+++ test/test.js
@@ -3,7 +3,47 @@ var throttleTransfer = require('..')
 describe('throttle test case', function () {
     var fn
     var throttle
-    it('test throttle 1', function () {
-        expect(true).toBe(true)
+    var firstCallWait = false
+    beforeEach(function () {
+        fn = jasmine.createSpy('fn')
+        // throttle = throttleTransfer(fn, 600, firstCallWait)
+        jasmine.clock().install()
+    })
+    afterEach(function () {
+        jasmine.clock().uninstall()
+    })
+
+    it('test throttle with firstcall immediately excute', function () {
+        firstCallWait = false
+        throttle = throttleTransfer(fn, 600, firstCallWait)
+        throttle()
+        jasmine.clock().tick(600)
+        expect(fn).toHaveBeenCalledTimes(2)
+    })
+
+    it('test throttle with firstcall excute after 600ms', function () {
+        firstCallWait = true
+        throttle = throttleTransfer(fn, 600, firstCallWait)
+        throttle()
+        jasmine.clock().tick(600)
+        expect(fn).toHaveBeenCalledTimes(1)
+    })
+
+    it('test throttle multiple during 600ms with  immediately excute', function () {
+        firstCallWait = false
+        throttle = throttleTransfer(fn, 600, firstCallWait)
+        throttle()
+        throttle()
+        jasmine.clock().tick(600)
+        expect(fn).toHaveBeenCalledTimes(2)
+    })
+
+    it('test throttle multiple during 600ms with  immediately excute', function () {
+        firstCallWait = true
+        throttle = throttleTransfer(fn, 600, firstCallWait)
+        throttle()
+        throttle()
+        jasmine.clock().tick(600)
+        expect(fn).toHaveBeenCalledTimes(1)
     })
 })
